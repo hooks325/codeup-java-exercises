38a6774e650d51441d7838432ba1dbb42af3ad64
nedwaldie <nw_waldie3@yahoo.com>
2017-01-03 12:36:15 -0600

    First commit for codeup-java-exercises

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by nedwaldie on 1/3/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("All java code lives in classes. ");
+        System.out.println("Goodbye, World!");
+    }
+}
